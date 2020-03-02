4968f6fbbe74d92e9c2c7084ba321feef30dcdd2
imhele <work@imhele.com>
2020-03-02 23:23:42 +0800

    chore(build): use tsc instead of father

--- .fatherrc.ts
+++
@@ -1,9 +0,0 @@
-import { IBundleOptions } from 'father';
-
-const options: IBundleOptions = {
-  cjs: 'babel',
-  esm: { type: 'babel', importLibToEs: true },
-  lessInBabelMode: true,
-};
-
-export default options;
