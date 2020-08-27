d05a92efc1f9247d4ce5513d80ca6f975fd1acec
imhele <work@imhele.com>
2020-08-27 23:35:56 +0800

    wip(jsx): update example

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     'no-restricted-syntax': 0,
     'no-unused-expressions': 0,
     'prefer-destructuring': 0,
+    'import/no-named-as-default': 0,
     'react/no-unknown-property': 0,
   },
   settings: {
